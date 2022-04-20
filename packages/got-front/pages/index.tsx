ceb99dd33cf835aa5000045fc0a77733b926978d
ireshka <ireshka@users.noreply.github.com>
2022-04-20 11:17:21 +0200

    feat(got-front): connect <BoardWidget /> with home page

--- packages/got-front/pages/index.tsx
+++ packages/got-front/pages/index.tsx
@@ -1,21 +1,23 @@
-import styled from '@emotion/styled';
+import { Box, Container, Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import { BoardWidget } from '../components/BoardWidget';
 
-const StyledPage = styled.div`
-  .page {
-  }
-`;
-
-export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.@emotion/styled file.
-   */
+const Home: NextPage = () => {
   return (
-    <StyledPage>
-      {'Clear app'}
-    </StyledPage>
+    <Container maxWidth="md">
+      <Typography
+        variant="h3"
+        component="h1"
+        textAlign="center"
+        sx={{ p: '2rem 2rem' }}
+      >
+        Game of Typescript Life
+      </Typography>
+      <Box sx={{ width: '100%' }}>
+        <BoardWidget />
+      </Box>
+    </Container>
   );
-}
+};
 
-export default Index;
+export default Home;
